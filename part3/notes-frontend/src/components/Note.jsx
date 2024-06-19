f35e515b690cb68bfe23645fb6bb8e9d589e8668
Angel Ivanov <angel.iwanov@gmail.com>
2024-06-19 19:30:43 +0300

    part5.d playwright-2

--- part3/notes-frontend/src/components/Note.jsx
+++ part3/notes-frontend/src/components/Note.jsx
@@ -3,7 +3,7 @@ const Note = ({ note, toggleImportance, handleDelete }) => {
 
   return (
     <li className="note">
-      {note.content}
+      <span>{note.content}</span>
       <button onClick={toggleImportance}>{label}</button>
       <button onClick={handleDelete}>delete</button>
     </li>
--- part5/playwright/tests/note_app.spec.js
+++ part5/playwright/tests/note_app.spec.js
@@ -1,10 +1,10 @@
 const { test, describe, expect, beforeEach } = require("@playwright/test");
-const exp = require("constants");
+const { loginWith, createNote } = require("./helper");
 
 describe("Note app", () => {
   beforeEach(async ({ page, request }) => {
-    await request.post("http://localhost:3001/api/testing/reset");
-    await request.post("http://localhost:3001/api/users", {
+    await request.post("/api/testing/reset");
+    await request.post("/api/users", {
       data: {
         name: "Angel Ivanov",
         username: "angelivanov",
@@ -12,7 +12,7 @@ describe("Note app", () => {
       },
     });
 
-    await page.goto("http://localhost:5173");
+    await page.goto("/");
   });
 
   test("front page can be opened", async ({ page }) => {
@@ -22,25 +22,28 @@ describe("Note app", () => {
   });
 
   test("user can log in", async ({ page }) => {
-    await page.getByRole("button", { name: "log in" }).click();
-    await page.getByTestId("username").fill("angelivanov");
-    await page.getByTestId("password").fill("123456");
-    await page.getByRole("button", { name: "login" }).click();
+    await loginWith(page, "angelivanov", "123456");
     await expect(page.getByText("Angel Ivanov logged")).toBeVisible();
+  });
+
+  test("login fails with wrong password", async ({ page }) => {
+    await loginWith(page, "angelivanov", "wrong");
+
+    const errorDiv = page.locator(".error");
+    await expect(errorDiv).toContainText("Wrong credentials");
+    await expect(errorDiv).toHaveCSS("border-style", "solid");
+    await expect(errorDiv).toHaveCSS("color", "rgb(255, 0, 0)");
+
+    await expect(page.getByText("Angel Ivanov logged")).not.toBeVisible();
   });
 
   describe("when loggen in", () => {
     beforeEach(async ({ page }) => {
-      await page.getByRole("button", { name: "log in" }).click();
-      await page.getByTestId("username").fill("angelivanov");
-      await page.getByTestId("password").fill("123456");
-      await page.getByRole("button", { name: "login" }).click();
+      await loginWith(page, "angelivanov", "123456");
     });
 
     test("a new note can be created", async ({ page }) => {
-      await page.getByRole("button", { name: "new note" }).click();
-      await page.getByRole("textbox").fill("a note created by playwright");
-      await page.getByRole("button", { name: "save" }).click();
+      await createNote(page, "a note created by playwright");
       await expect(
         page.getByText("a note created by playwright")
       ).toBeVisible();
@@ -48,14 +51,29 @@ describe("Note app", () => {
 
     describe("and a note exists", () => {
       beforeEach(async ({ page }) => {
-        await page.getByRole("button", { name: "new note" }).click();
-        await page.getByRole("textbox").fill("another note by playwright");
-        await page.getByRole("button", { name: "save" }).click();
+        await createNote(page, "first note");
+        await createNote(page, "second note");
+        await createNote(page, "third note");
       });
 
       test("importance can be changed", async ({ page }) => {
-        await page.getByRole("button", { name: "make not important" }).click();
-        await expect(page.getByText("show all")).toBeVisible();
+        await page.pause();
+
+        // await page
+        //   .locator("li")
+        //   .filter({ hasText: "second" })
+        //   .getByRole("button");
+
+        const otherNoteText = await page.getByText("second note");
+        const otherNoteElement = await otherNoteText.locator("..");
+        await otherNoteElement
+          .getByRole("button", { name: "make not important" })
+          .click();
+
+        await page.getByRole("button", { name: "show all" }).click();
+        await expect(
+          otherNoteElement.getByText("make important")
+        ).toBeVisible();
       });
     });
   });
